5acb22f8fd8be0b5fafc743e395b70368b54a3a6
Xufang Deng <xudeng@luc.edu>
2020-02-22 23:11:22 -0600

    scripts to extract reference genome

---
+++ extract_reference_genome.py
@@ -0,0 +1,20 @@
+#import Entrez module to retrive data from NCBI
+from Bio import Entrez
+from Bio import SeqIO
+#Fill in the Entrez.email field
+Entrez.email = "xudeng@luc.edu.com"
+
+
+#retrive data by searching the Nucleotide database with term
+handle = Entrez.efetch(db="nucleotide", id='EF999921', rettype="gb", retmode="text")
+
+with open("reference_cDNA.txt",'w') as outfile:
+    for record in SeqIO.parse(handle,"genbank"):
+        if record.features:
+            for feature in record.features:
+                if feature.type == 'CDS':
+                    pdtlist = feature.qualifiers["product"]
+                    outfile.write(">"+pdtlist[0]+'\n')
+                    CDSlist = feature.location.extract(record).seq
+                    outfile.write(str(CDSlist)+'\n')
+    outfile.close()
