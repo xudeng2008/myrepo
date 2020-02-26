385d85f0dbf7e314585d00ccb7b5bb599e146c92
Xufang Deng <xudeng@luc.edu>
2020-02-26 13:45:43 -0600

    modify SPAdes script

---
+++ SPAdes_assembly.py
@@ -0,0 +1,7 @@
+import os
+os.system('cat miniProject_Xufang_Deng/results/*mapped.1.fq > merged_1.fq') #merge all 4 forward file to  a single file
+os.system('cat miniProject_Xufang_Deng/results/*mapped.2.fq > merged_2.fq') #merge all 4 reverse file to  a single file
+os.system('spades -k 55,77,99,127 -t 2 --only-assembler -1 miniProject_Xufang_Deng/results/merged_1.fq -2 miniProject_Xufang_Deng/results/merged_2.fq  -o HCMV_assembly') #use SPAdes to assembly 
+
+with open ('miniProject.log','a') as log:
+  log.write('spades -k 55,77,99,127 -t 2 --only-assembler -1 miniProject_Xufang_Deng/results/merged_1.fq -2 miniProject_Xufang_Deng/results/merged_2.fq -o HCMV_assembly\n\n')
