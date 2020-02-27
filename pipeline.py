b12246522097d001e7b4c758bbcab9d4d8c30309
xudeng2008 <58059010+xudeng2008@users.noreply.github.com>
2020-02-26 23:10:05 -0600

    debug

--- pipeline.py
+++ pipeline.py
@@ -36,12 +36,12 @@ log.close()
 
 #Use kallisto to quantify the reads
 os.system('mkdir index')
-os.system('time kallisto index -i index/index.idx miniProject_Xufang_Deng/reference_cDNA.fasta') #build an index file
+os.system('time kallisto index -i index.idx miniProject_Xufang_Deng/reference_cDNA.fasta') #build an index file
 
 with open ("SRR_accession_IDs.txt",'r') as infile: #read in the accession IDs
     IDlist = infile.read().splitlines() #make the IDs as list
     for id in IDlist:
-        os.system ('time kallisto quant -i index/index.idx -o miniProject_Xufang_Deng/results/'+id+' -b 30 -t 4 miniProject_Xufang_Deng/'+id+'_1.fastq'+'miniProject_Xufang_Deng/'+id+'_2.fastq') #run kallisto function to quantify reads
+        os.system ('time kallisto quant -i index.idx -o miniProject_Xufang_Deng/results/'+id+' -b 30 -t 4 miniProject_Xufang_Deng/'+id+'_1.fastq'+'miniProject_Xufang_Deng/'+id+'_2.fastq') #run kallisto function to quantify reads
 
 #run sleuth Rscript to calculate differential expression
 os.system('Rscript scripts/sleuth.R') #store the significant genes in a DEGs.txt file
