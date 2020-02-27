9bc4b38692b6c3a394947259512dd123c707c35c
xudeng2008 <58059010+xudeng2008@users.noreply.github.com>
2020-02-27 14:01:39 -0600

    Update and rename extract_reference_genome.py to extract_reference_cDNA.py

--- scripts/extract_reference_cDNA.py
+++ scripts/extract_reference_cDNA.py
@@ -3,17 +3,19 @@ from Bio import SeqIO #import SeqIO module
 
 #Fill in the Entrez.email field
 Entrez.email = "xudeng@luc.edu"
+
 #retrive data by searching the Nucleotide database with term
 handle = Entrez.efetch(db="nucleotide", id='EF999921', rettype="gb", retmode="text")
-with open("reference_cDNA.fasta",'w') as outfile: #create an out file
-    count = 0 #store the number of CDS
+with open("miniProject_Xufang_Deng/reference_cDNA.fasta",'w') as outfile: #create an out file
+#    record = SeqIO.parse(handle,"genbank")
+#    record.features = [f for f in record.features if f.type == "CDS"]
+#    SeqIO.write(record, outfile, "genbank")
+#    outfile.close()
     for record in SeqIO.parse(handle,"genbank"): #parse the file
         if record.features: #if record has features
             for feature in record.features: #loop the features
                 if feature.type == 'CDS': #find the CDS feature
-                    pdtlist = feature.qualifiers["product"] #get the product name
-                    outfile.write(">"+pdtlist[0]+'\n') #store the name
-                    CDSlist = feature.location.extract(record).seq #get the CDS sequence
-                    outfile.write(str(CDSlist)+'\n')
-                    count+=1
+                    outfile.write(">"+str(feature.qualifiers["protein_id"]).replace("[","").replace("]","").replace("'","")+'\n') #store the name of the CDS
+                    CDS = feature.location.extract(record).seq #get the CDS sequence
+                    outfile.write(str(CDS)+'\n')
     outfile.close()
