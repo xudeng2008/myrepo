daf56ffe4a3706edf2f5533e8fab3cad354b12de
xudeng2008 <58059010+xudeng2008@users.noreply.github.com>
2020-02-26 22:56:48 -0600

    Add more comments and modify file paths

--- pipeline.py
+++ pipeline.py
@@ -56,13 +56,13 @@ log.close()
 with open ("SRR_accession_IDs.txt",'r') as infile: #read in the accession IDs
     IDlist = infile.read().splitlines() #make the IDs as list
 
-    os.system('bowtie2-build reference_cDNA.fasta index/HCMV') #build an index file
+    os.system('bowtie2-build miniProject_Xufang_Deng/reference_cDNA.fasta index/HCMV') #build an index file
 
     for id in IDlist:
-        os.system("bowtie2 --quiet -x index/HCMV -1 miniProject_Xufang_Deng/"+id+"_1.fastq"+" -2 miniProject_Xufang_Deng/"+id+"_2.fastq --al-conc miniProject_Xufang_Deng/results/"+id+"mapped.fq") #map reads to a reference
+        os.system("bowtie2 --quiet -x index/HCMV -1 miniProject_Xufang_Deng/"+id+"_1.fastq"+" -2 miniProject_Xufang_Deng/"+id+"_2.fastq --al-conc miniProject_Xufang_Deng/"+id+"mapped.fq") #map reads to a reference
 
 
-#define a function to count the number of reads
+#define a function to count the number of reads in a file
 def count_read(infile):
     count = 0
     for line in infile:
@@ -74,26 +74,26 @@ def count_read(infile):
 with open ("miniProject.log",'a') as log:
     with open ("SRR_accession_IDs.txt",'r') as infile: #read in the accession IDs
         IDlist = infile.read().splitlines() #make the IDs as list
-        donorIDs = ['1','1','3','3']
-        timePoints = ['2','6','2','6']
+        donorIDs = ['1','1','3','3'] #donors' IDs
+        timePoints = ['2','6','2','6'] #time points
         i = 0
         for id in IDlist:
             infile1 = open('miniProject_Xufang_Deng/'+id+'_1.fastq','r')
-            total_raw = count_read(infile1)
+            total_raw = count_read(infile1) #get the total number of reads
             
-            infile2 = open('miniProject_Xufang_Deng/results/'+id+'mapped.1.fq','r')
+            infile2 = open('miniProject_Xufang_Deng/'+id+'mapped.1.fq','r')
             total_mapped = count_read(infile2)
-            log.write("Donor "+donorIDs[i]+" at "+timePoints[i]+" dpi had "+str(total_raw)+" read pairs before Bowtie2 filtering and "+str(total_mapped)+" read after.\n\n")
+            log.write("Donor "+donorIDs[i]+" at "+timePoints[i]+" dpi had "+str(total_raw)+" read pairs before Bowtie2 filtering and "+str(total_mapped)+" read after.\n")
             i+=1
 log.close()
 
 #Use SPAdes to assemble mapped reads
-os.system('cat miniProject_Xufang_Deng/results/*mapped.1.fq > merged_1.fq') #merge all 4 forward file to  a single file
-os.system('cat miniProject_Xufang_Deng/results/*mapped.2.fq > merged_2.fq') #merge all 4 reverse file to  a single file
-os.system('spades -k 55,77,99,127 -t 2 --only-assembler -1 miniProject_Xufang_Deng/results/merged_1.fq -2 miniProject_Xufang_Deng/results/merged_2.fq  -o HCMV_assembly') #use SPAdes to assembly 
+os.system('cat miniProject_Xufang_Deng/*mapped.1.fq > merged_1.fq') #merge all 4 forward file to  a single file
+os.system('cat miniProject_Xufang_Deng/*mapped.2.fq > merged_2.fq') #merge all 4 reverse file to  a single file
+os.system('spades -k 55,77,99,127 -t 2 --only-assembler -1 miniProject_Xufang_Deng/merged_1.fq -2 miniProject_Xufang_Deng/merged_2.fq  -o HCMV_assembly') #use SPAdes to assembly 
 
 with open ("miniProject.log",'a') as log:
-    log.write('spades -k 55,77,99,127 -t 2 --only-assembler -1 miniProject_Xufang_Deng/results/merged_1.fq -2 miniProject_Xufang_Deng/results/merged_2.fq -o HCMV_assembly\n\n')
+    log.write('spades -k 55,77,99,127 -t 2 --only-assembler -1 miniProject_Xufang_Deng/merged_1.fq -2 miniProject_Xufang_Deng/merged_2.fq -o HCMV_assembly\n\n')
 log.close()
 
 #count the number of contig with a length greater than 1000 nt
@@ -109,12 +109,12 @@ with open("HCMV_assembly/contigs.fasta",'r') as infile:
                 totalLen += ctgLen
                 SeqIO.write(record,outfile, 'fasta') #put the contigs longer than 1000bp in a file
 with open ("miniProject.log",'a') as log:
-    log.write("There are "+str(count)+" contigs > 1000 bp in the assembly.\n\n")
+    log.write("There are "+str(count)+" contigs > 1000 bp in the assembly.\n")
     log.write("There are "+str(totalLen)+" bp in the assembly.\n\n")
 log.close()
 
 #concatenate all of the contigs > 1000 bp with a linker
-#build a linker string
+#build a linker string with 50 Ns
 linker = ''
 while len(linker)<50:
     linker += 'N'
@@ -126,27 +126,28 @@ with open('HCMV_assembly/contigs_gt_1000.fasta','r') as infile:
         for record in SeqIO.parse(infile, 'fasta'):
             currCtg = str(record.seq)+linker #add the linker
             superString += currCtg
-    outfile.write(">superString\n"+superString[:len(superString)-50]) #remove the last linker from the string
+    outfile.write(">superString\n"+superString[:len(superString)-50]) #remove the last linker from the end of the superstring
 outfile.close()
 
 #Blast the assembled sequence in NCBI
-query_seq = open('ligated_contigs.fasta').read()
-result_handle = NCBIWWW.qblast("blastn", "nt", query_seq, entrez_query = "Herpesviridae", hitlist_size= 10)
+query_seq = open('HCMV_assembly/ligated_contigs.fasta').read() # read in the assembled contigs
+result_handle = NCBIWWW.qblast("blastn", "nt", query_seq, entrez_query = "Herpesviridae", hitlist_size= 10) # run blastn in nt database
 
-with open ("miniProject.log",'a') as log:
-    log.write("seq_title\talign_len\tnumber_HSPs\ttopHSP_ident\ttopHSP_gaps\ttopHSP_bits\ttopHSP_expect")
+with open ("miniProject.log",'a') as log: #read in the log file
+    log.write("seq_title\talign_len\tnumber_HSPs\ttopHSP_ident\ttopHSP_gaps\ttopHSP_bits\ttopHSP_expect") # head of the log file of blast report
 
-blast_records = NCBIXML.parse(result_handle)
-blast_records = list(blast_records)
-for record in blast_records:
-    for alignment in record.alignments:
-        seq_title= alignment.title
-        align_len= str(alignment.length)
-        number_HSPs= str(len(alignment.hsps))
-        topHSP_ident= str(alignment.hsps[0].identities)
-        topHSP_gaps= str(alignment.hsps[0].gaps)
-        topHSP_bits= str(alignment.hsps[0].bits)
-        topHSP_expect= str(alignment.hsps[0].expect)
+blast_records = NCBIXML.parse(result_handle) #Use NCBIXML to parse the blast output
+blast_records = list(blast_records) #convert the output as a list
+for record in blast_records: #loop the list of blast results
+    for alignment in record.alignments: #loop each alignment
+        seq_title= alignment.title #get the subject sequence title
+        align_len= str(alignment.length) # alignment length
+        number_HSPs= str(len(alignment.hsps)) # number of high-scoring segments in the alignment
+        topHSP_ident= str(alignment.hsps[0].identities) # the identity between query sequence and subjected segment
+        topHSP_gaps= str(alignment.hsps[0].gaps) # find the gaps of the alignment
+        topHSP_bits= str(alignment.hsps[0].bits) # bit-score of the alignment
+        topHSP_expect= str(alignment.hsps[0].expect) # expect value of the alignment
+        # write these output in log file
         log.write(seq_title+"\ta"+lign_len+"\t"+number_HSPs+"\t"+topHSP_ident+"\t"+topHSP_gaps+"\t"+topHSP_bits+"\t"+topHSP_expect)
-
+#close the log file
 log.close()
