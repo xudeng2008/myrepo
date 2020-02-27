8ced12537341afa5efd0175a56b927832bac0719
xudeng2008 <58059010+xudeng2008@users.noreply.github.com>
2020-02-26 18:49:01 -0600

    Delete kallisto_quant.py

--- kallisto_quant.py
+++
@@ -1,5 +0,0 @@
-import os
-IDlist = ['SRR5660030','SRR5660033','SRR5660044','SRR5660045']
-for id in IDlist:
-        os.system ('time kallisto quant -i index.idx -o /homes/xudeng/myrepo/miniProject_Xufang_Deng/results/'+id+' -b 30 -t 4 /homes/xudeng/myrepo/miniProject_Xufang_Deng/'+id+'_1.fastq'+' /homes/xudeng/myrepo/miniProject_Xufang_Deng/'+id+'_2.fastq')
-
