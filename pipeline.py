ecaaf42dd35ae52163f89d90e5cc150010a3f647
xudeng2008 <58059010+xudeng2008@users.noreply.github.com>
2020-02-27 13:12:22 -0600

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -12,7 +12,7 @@ os.system("mkdir miniProject_Xufang_Deng") # A directory to store downloaded dat
 with open ("SRR_accession_IDs.txt",'r') as infile: #read in the accession IDs
     IDlist = infile.read().splitlines() #make the IDs as list
     for id in IDlist:
-        os.system('fastq-dump -I --split-files '+id+" -O /miniProject_Xufang_Deng") #use fastq-dump to get splited fastq files
+        os.system('fastq-dump -I --split-files '+id) #use fastq-dump to get splited fastq files
 
 
 #Fill in the Entrez.email field
