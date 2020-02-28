8f74646de418acb286b62f108a292431aac3a130
xudeng2008 <58059010+xudeng2008@users.noreply.github.com>
2020-02-27 23:22:56 -0600

    Update kallisto_quant.py

--- scripts/kallisto_quant.py
+++ scripts/kallisto_quant.py
@@ -1,9 +1,9 @@
 import os
-#Use kallisto to quantify the reads
-os.system('time kallisto index -i index/index.idx reference_cDNA.fasta') #build an index file
+
+os.system('mkdir index_test')
+os.system('time kallisto index -i index_test/index.idx '+path+'reference_cDNA.fasta') #build an index file
 
 with open ("SRR_accession_IDs.txt",'r') as infile: #read in the accession IDs
     IDlist = infile.read().splitlines() #make the IDs as list
     for id in IDlist:
-        os.system ('time kallisto quant -i index/index.idx -o /miniProject_Xufang_Deng/results/'+id+' -b 30 -t 4 /myrepo/miniProject_Xufang_Deng/'+id+'_1.fastq'+'/miniProject_Xufang_Deng/'+id+'_2.fastq') #run kallisto function to quantify reads
-
+        os.system ('time kallisto quant -i index_test/index.idx -o '+path+id+' -b 30 -t 4 '+path+id+'_1.fastq '+path+id+'_2.fastq') #run kallisto function to quantify reads
