f547d12e669c97b88e6bea57fc8ed309d5b750c1
xudeng2008 <58059010+xudeng2008@users.noreply.github.com>
2020-02-27 23:18:29 -0600

    Update SPAdes_assembly.py

--- scripts/SPAdes_assembly.py
+++ scripts/SPAdes_assembly.py
@@ -1,6 +1,8 @@
 import os
 
-os.system('cat miniProject_Xufang_Deng/results/*mapped.1.fq > merged_1.fq') #merge all 4 forward file to  a single file
-os.system('cat miniProject_Xufang_Deng/results/*mapped.2.fq > merged_2.fq') #merge all 4 reverse file to  a single file
-os.system('spades -k 55,77,99,127 -t 2 --only-assembler -1 miniProject_Xufang_Deng/results/merged_1.fq -2 miniProject_Xufang_Deng/results/merged_2.fq  -o HCMV_assembly') #use SPAdes to assembly 
+#Use SPAdes to assemble mapped reads
+os.system('cat '+path+'*mapped.1.fq > '+path+'merged_1.fq') #merge all 4 forward file to  a single file
+os.system('cat '+path+'*mapped.2.fq > '+path+'merged_2.fq') #merge all 4 reverse file to  a single file
+os.system('spades -k 55,77,99,127 -t 4 --only-assembler -1 '+path+'merged_1.fq -2 '+path+'merged_2.fq  -o HCMV_assembly_test') #use SPAdes to assembly 
+
 
