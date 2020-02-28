40dbc620709ab9bc758b1e3b102efc33776b6f77
xudeng2008 <58059010+xudeng2008@users.noreply.github.com>
2020-02-27 23:20:15 -0600

    Update bowtie2_mapping.py

--- scripts/bowtie2_mapping.py
+++ scripts/bowtie2_mapping.py
@@ -4,8 +4,9 @@ import os
 with open ("SRR_accession_IDs.txt",'r') as infile: #read in the accession IDs
     IDlist = infile.read().splitlines() #make the IDs as list
 
-    os.system('bowtie2-build reference_cDNA.fasta index/HCMV') #build an index file
+    os.system('bowtie2-build '+path+'reference_genome.fasta index_test/HCMV_test') #build an index file
 
     for id in IDlist:
-        os.system("bowtie2 --quiet -x index/HCMV -1 miniProject_Xufang_Deng/"+id+"_1.fastq"+" -2 miniProject_Xufang_Deng/"+id+"_2.fastq --al-conc al-conc miniProject_Xufang_Deng/results/"+id+"mapped.fq") #map reads to a reference
+        os.system("bowtie2 --quiet -x index_test/HCMV_test -1 "+path+id+"_1.fastq"+" -2 "+path+id+"_2.fastq --al-conc "+path+id+"mapped.fq") #map reads to a reference
+
 
